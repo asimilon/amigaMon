f20419c433167748f8792e918c9b0d5e67d54658
Rich Elmes <rich.elmes@a-i-media.co.uk>
2024-11-19 16:19:23 +0100

    Fix stupid copy/paste error

--- CMake/CMakeCommon.cmake
+++ CMake/CMakeCommon.cmake
@@ -100,7 +100,7 @@ function(addGuiApp appName AppSourceFiles Modules SourceFolders iconPath juceOpt
 
     set_target_properties("${appName}" PROPERTIES XCODE_GENERATE_SCHEME ON)
 
-    target_compile_options(your_target PRIVATE 
+    target_compile_options("${appName}" PRIVATE 
         $<$<CXX_COMPILER_ID:GNU>:-Wno-error>       # For GCC
         $<$<CXX_COMPILER_ID:Clang>:-Wno-error>      # For Clang
         $<$<CXX_COMPILER_ID:MSVC>:/wd4996>          # For MSVC, example to disable a specific warning
